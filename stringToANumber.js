ba15875dc1e0da70855ed0306fb578b3743a30e5
jmlengineering <jasmineleeengineering@gmail.com>
2022-05-24 08:39:08 -0400

    Initial commit

---
+++ stringToANumber.js
@@ -0,0 +1,13 @@
+/* 
+
+Date: May 24, 2022
+Title: Convert a String to a Number
+Instructions: We need a function that can transform a string into a number. 
+
+*/
+
+const stringToNumber = function(str){
+    //The parse function simply parses a string, which is a process of analyzing and transforming, into an integer.
+    var num = parseInt(str);
+    return num;
+}
---
+++ string_to_a_number.py
@@ -0,0 +1,11 @@
+"""
+Date: May 24, 2022
+Title: Convert a String to a Number
+Instructions: We need a function that can transform a string into a number. 
+
+"""
+
+def string_to_number(s):
+    #The int() function simply converts the given value into an integer.
+    x = int(s)
+    return x
