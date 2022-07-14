1a96ead8d19ea715c55fc3b8740233714e3450ab
Jasmine M. Lee <94409703+jmlengineering@users.noreply.github.com>
2022-07-14 16:55:39 -0400

    Create Result.java

---
+++ Result.java
@@ -0,0 +1,20 @@
+class Result {
+
+    /*
+     * Complete the 'simpleArraySum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY ar as parameter.
+     */
+
+    public static int simpleArraySum(List<Integer> ar) {
+    // Write your code here
+    int sum = 0;
+    for(int i: ar) {
+        sum += i;
+    }
+    return sum;
+
+    }
+
+}
